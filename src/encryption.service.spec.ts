69b29e361ff9af19156feeb58e1e608296f0ac93
Jared Porcenaluk <jporcenaluk@gmail.com>
2020-03-27 08:25:14 -0400

    now can encrypt a message from a buffer

--- src/encryption.service.spec.ts
+++ src/encryption.service.spec.ts
@@ -6,18 +6,20 @@ import crypto from "crypto";
 
 it("encrypts in a way we can read from", () => {
     // Arrange
-    const secretKey = crypto.randomBytes(32);
+    const secretKey = crypto.randomBytes(32).toString('base64');
     const message = "message";
+    const messageBuffer = Buffer.from("message", 'utf8');
     const initializationVector = crypto.randomBytes(16);
-    const algorithm = 'aes-256-cbc';
+
     // Act
-    const encrypted = Encryption.encrypt(message, secretKey, initializationVector);
+    const encrypted = Encryption.encrypt(messageBuffer, secretKey, initializationVector);
 
     // Assert
-    let iv = Buffer.from(initializationVector);
-    let encryptedText = Buffer.from(encrypted);
-    let decipher = crypto.createDecipheriv(algorithm, Buffer.from(secretKey), iv);
-    let decrypted = decipher.update(encryptedText);
+    let iv = initializationVector;
+    const algorithm = 'aes-256-cbc';
+    const decodedKey = Buffer.from(secretKey, 'base64');
+    let decipher = crypto.createDecipheriv(algorithm, decodedKey, iv);
+    let decrypted = decipher.update(encrypted);
     decrypted = Buffer.concat([decrypted, decipher.final()]);
     const decryptedMessage = decrypted.toString();
     expect(decryptedMessage).to.equal(message);
--- src/encryption.service.ts
+++ src/encryption.service.ts
@@ -1,7 +1,8 @@
 import * as crypto from "crypto";
 
-export function encrypt(value: string, key: Buffer, initializationVector: Buffer) {
-    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), initializationVector);
+export function encrypt(value: Buffer, base64EncodedKey: string, initializationVector: Buffer) {
+    const decodedKey = decodeKey(base64EncodedKey);
+    let cipher = crypto.createCipheriv('aes-256-cbc', decodedKey, initializationVector);
     let encrypted = cipher.update(value);
     encrypted = Buffer.concat([encrypted, cipher.final()]);
     return encrypted;
@@ -10,4 +11,9 @@ export function encrypt(value: string, key: Buffer, initializationVector: Buffer
 export function getInitializationVector() {
     const initializationVector = crypto.randomBytes(16).toString('utf-8');
     return initializationVector;
+}
+
+export function decodeKey(base64EncodedKey: string): Buffer {
+    const decoded = Buffer.from(base64EncodedKey, 'base64');
+    return decoded;
 }
